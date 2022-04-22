152980ed46187ae3a55ade2369f0125e54bfe00b
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2022-04-22 13:55:50 +0900

    スクリプト追加

---
+++ Assets/Scripts/EcColliderController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EcColliderController : MonoBehaviour
+{
+    public bool counterHetSwicth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "playerCounterattack")
+        {
+            //Debug.Log("ƒqƒbƒg");
+            counterHetSwicth = true;
+        }
+    }
+}
