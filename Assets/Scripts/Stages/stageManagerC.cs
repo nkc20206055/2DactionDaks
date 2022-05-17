847ab866b3a103c72a613f5444678d8d41a4d47c
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2022-05-17 15:15:22 +0900

    Stage1更新

--- Assets/Scripts/Stages/stageManagerC.cs
+++ Assets/Scripts/Stages/stageManagerC.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class stageManagerC : MonoBehaviour
 {
@@ -9,14 +10,32 @@ public class stageManagerC : MonoBehaviour
     [SerializeField] GameObject GameoverUI;//Gameoverで出現するUI
     public GameObject stopUIhaikei;
 
-    public bool normalSwicth;
+    //public float SavePosY;//セーブポイントのY座標保存用
+    public string retrySceneName;//リトライした時に行くSceneの名前をいれる
+    public string GOSceneName;//ゲームオーバーした時に行くSceneの名前をいれる
+    public bool normalSwicth;//お試しなどのSceneではtrueにしておく
     public bool pauseSwicth;
 
-    GameObject /*playerG,*/ bossG;
+    GameObject playerG, bossG;
     groundController gC;
 
     private Image stopI;
     private bool stopmodeSwicth, DestroyMi;
+
+    //ボタン操作用
+    public void Retry()//リトライ
+    {
+        //PlayerPrefs.Save();//"SaveXpos","SaveYpos"を保存
+        SceneManager.LoadScene(retrySceneName);
+    }
+    public void GameOver()//ゲームオーバー
+    {
+        PlayerPrefs.DeleteKey("SaveXpos");//キーの削除
+        PlayerPrefs.DeleteKey("SaveYpos");//キーの削除
+        //SceneManager.LoadScene(GOSceneName);
+    }
+
+
     void stopmode()//一時停止
     {
         if (Input.GetKeyDown(KeyCode.Tab))
@@ -56,11 +75,30 @@ public class stageManagerC : MonoBehaviour
     void Start()
     {
         if (normalSwicth==false) {
-            gC = GameObject.FindWithTag("Player").GetComponent<groundController>();
+            playerG = GameObject.FindWithTag("Player");
+            gC = playerG.GetComponent<groundController>();
             //bossG = GameObject.FindWithTag("boss");
             stopI = stopUIhaikei.GetComponent<Image>();
             stopmodeSwicth = false;
             DestroyMi = false;
+
+            //PlayerPrefs.SetString("SaveXpos","SaveYpos");//位置のセーブ用
+            if (PlayerPrefs.HasKey("SaveXpos") && PlayerPrefs.HasKey("SaveYpos"))//セーブがあったら
+            {
+                Debug.Log("SaveXposデータとSaveYposデータは存在します");
+                Vector3 pos = new Vector3(PlayerPrefs.GetFloat("SaveXpos"), PlayerPrefs.GetFloat("SaveYpos"), 0);
+                playerG.transform.position = pos;
+            }
+            else//セーブがなかったら
+            {
+                Debug.Log("SaveXposデータとSaveYposデータは存在しません");
+                Vector2 Ppos = new Vector2(playerG.transform.position.x, 
+                                           playerG.transform.position.y);
+                PlayerPrefs.SetString("SaveXpos", "SaveYpos");//位置のセーブ用
+                PlayerPrefs.SetFloat("SaveXpos", Ppos.x);
+                PlayerPrefs.SetFloat("SaveYpos", Ppos.y);
+                playerG.transform.position = new Vector3(Ppos.x,Ppos.y,0);
+            }
         }
 
         pauseSwicth = false;
@@ -77,6 +115,33 @@ public class stageManagerC : MonoBehaviour
             } else if (gC.hp>0) {
                 stopmode();
             }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Delete))//PlayerPrefsは勝手に保存されているんで注意
+        {
+            PlayerPrefs.DeleteKey("SaveXpos");//キーの削除
+            PlayerPrefs.DeleteKey("SaveYpos");//キーの削除
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag=="Player")//セーブポイント用にプレイヤーの位置を保存する
+        {
+            Debug.Log("ヒット");
+            PlayerPrefs.DeleteKey("SaveXpos");//キーの削除
+            PlayerPrefs.DeleteKey("SaveYpos");//キーの削除
+            Vector2 savepos = collision.gameObject.transform.position;
+            PlayerPrefs.SetFloat("SaveXpos", savepos.x);
+            PlayerPrefs.SetFloat("SaveYpos", savepos.y);
+            if (PlayerPrefs.HasKey("SaveXpos") && PlayerPrefs.HasKey("SaveYpos"))
+            {
+                Debug.Log("SaveXposデータとSaveYposデータは存在します");
+            }
+            //if (PlayerPrefs.HasKey("SaveYpos"))
+            //{
+            //    Debug.Log("SaveYposデータは存在します");
+            //}
+
         }
     }
 }
