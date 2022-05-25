c1d63d9b52250447d16069383d1363dfcdca39dc
nkc20206055 <ym2022@mailg.denpa.ac.jp>
2022-05-25 15:09:46 +0900

    Stage1hell更新

--- Assets/Scripts/Stages/stage1ManagerC.cs
+++ Assets/Scripts/Stages/stage1ManagerC.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 
 public class stage1ManagerC : MonoBehaviour
 {
+    public Image TutorialUI;//チュートリアルで表示する文字画像
+
     GameObject playerG,BossG,CameraG;
     PlayerController PC;
     groundController gC;
@@ -13,26 +15,113 @@ public class stage1ManagerC : MonoBehaviour
     cameraController1 cC1;
     Boss1Controller B1C;
     stageManagerC sMC;
+    int TutorialNamber;//チュートリアルの順番を入れる
+    float tutorialTime, MaxtutorialTime;//チュートリアル説明の時間,チュートリアルの最大時間
+    float ImageFadeTime=0;
+    string tutorial;
     bool EventStratSwcith;
+    bool tutorialNSwicth/*,fadeSwicth*/;//TutorialNamber更新用スイッチ,フェードする場合に起動
+    public bool fadeSwicth;//フェードする場合に起動
+
+
+    void Imagefade()//チュートリアルの文字画像をフェードする
+    {
+        if (fadeSwicth==true)
+        {
+            if (ImageFadeTime<=255)
+            {
+                ImageFadeTime = 1f * Time.deltaTime;
+                TutorialUI.color += new Color(0, 0, 0, ImageFadeTime);
+            }
+        }
+        else
+        {
+            if (ImageFadeTime>=0)
+            {
+                ImageFadeTime = 1f * Time.deltaTime;
+                TutorialUI.color -= new Color(0, 0, 0, ImageFadeTime);
+            }
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        sMC = GetComponent<stageManagerC>();
-        playerG = GameObject.FindWithTag("Player");
-        PC= playerG.GetComponent<PlayerController>();
-        gC = playerG.GetComponent<groundController>();
-        JumpC= playerG.GetComponent<jump>();
-        BossG = GameObject.FindWithTag("boss");
-        B1C = BossG.GetComponent<Boss1Controller>();
-        CameraG = GameObject.Find("Main Camera");
-        cC1 = CameraG.GetComponent<cameraController1>();
+        sMC = GetComponent<stageManagerC>();                //自身のstageManagerCを取得
+        playerG = GameObject.FindWithTag("Player");         //タグでプレイヤーを取得
+        PC= playerG.GetComponent<PlayerController>();       //プレイヤーにあるPlayerControllerを取得
+        gC = playerG.GetComponent<groundController>();      //プレイヤーにあるgroundControllerを取得
+        JumpC = playerG.GetComponent<jump>();               //プレイヤーにあるjumpを取得
+        BossG = GameObject.FindWithTag("boss");             //タグでボスを取得
+        B1C = BossG.GetComponent<Boss1Controller>();        //ボスにあるBoss1Controllerを取得
+        CameraG = GameObject.Find("Main Camera");           //メインカメラを取得
+        cC1 = CameraG.GetComponent<cameraController1>();    //メインカメラにあるcameraController1を取得
+        TutorialNamber = 0;
+        MaxtutorialTime = 2;
         EventStratSwcith = true;
+        tutorialNSwicth = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (sMC.Eventnumber==1)//ボスイベント
+        if (sMC.Eventnumber == 0)//チュートリアル
+        {
+            Imagefade();
+
+            if (TutorialNamber==0)//移動
+            {
+                if (tutorialNSwicth == true)
+                {
+                    Debug.Log("移動");
+                    fadeSwicth = true;
+                    tutorialNSwicth = false;
+                }
+            }
+            else if (TutorialNamber == 1)//ジャンプ
+            {
+                if (tutorialNSwicth==true)
+                {
+
+                    tutorialNSwicth = false;
+                }
+            }
+            else if (TutorialNamber == 2)//攻撃
+            {
+                if (tutorialNSwicth == true)
+                {
+
+                    tutorialNSwicth = false;
+                }
+            }
+            else if (TutorialNamber == 3)//ガード
+            {
+                if (tutorialNSwicth == true)
+                {
+
+                    tutorialNSwicth = false;
+                }
+            }
+            else if (TutorialNamber == 4)//カウンター
+            {
+                if (tutorialNSwicth == true)
+                {
+
+                    tutorialNSwicth = false;
+                }
+            }
+
+            if (TutorialNamber<4&& TutorialNamber>4) {//チュートリアル表示時間
+                if (tutorialTime >= MaxtutorialTime)
+                {
+                    TutorialNamber++;
+                }
+                else
+                {
+                    tutorialTime += 1 * Time.deltaTime;
+                }
+            }
+        }
+        else if (sMC.Eventnumber==2)//ボスイベント
         {
             if (sMC.EventSwicth==true) {
                 if (CameraG.transform.position.x< 51.3f)//カメラがx軸の51.3fにたどり着いていなとき
